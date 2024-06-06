24dd0c9367fd3d991eae759985e50583b06cf98d
OksanaFedotova <oxana-fedotova@yandex.ru>
2024-06-07 00:32:24 +0400

    feat: specify sources for bucket

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,13 +1,19 @@
 import * as cdk from "aws-cdk-lib";
-import { aws_s3 as s3 } from "aws-cdk-lib";
+import { aws_s3 as s3, aws_s3_deployment as s3deploy } from "aws-cdk-lib";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class InfraStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new s3.Bucket(this, "BucketAwsShopReact", {
+    const siteBucket = new s3.Bucket(this, "BucketAwsShopReact", {
       versioned: true,
+      bucketName: "cdk-aws-oksana",
+      websiteIndexDocument: "index.html",
+    });
+    new s3deploy.BucketDeployment(this, "JSCC-Bucket-Deployment", {
+      sources: [s3deploy.Source.asset("../dist")],
+      destinationBucket: siteBucket,
     });
   }
 }
