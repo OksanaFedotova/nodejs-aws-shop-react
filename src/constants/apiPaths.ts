991826c284e1d56f9f4bc4ebeb21cdc2a9e4c551
OksanaFedotova <oxana-fedotova@yandex.ru>
2024-08-03 16:19:30 +0400

    feat: url protocol updated

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,9 +3,9 @@ const API_PATHS = {
   order: "https://qyflqg7cf4.execute-api.eu-west-1.amazonaws.com/prod",
   import: "https://lwn8xbwzfl.execute-api.eu-west-1.amazonaws.com/prod",
   bff: "https://qyflqg7cf4.execute-api.eu-west-1.amazonaws.com/prod",
-  cart: "http://oksanafedotova-cart-api-prod.eu-west-1.elasticbeanstalk.com/api",
-  user: "http://oksanafedotova-cart-api-prod.eu-west-1.elasticbeanstalk.com/api/users",
-  auth: "http://oksanafedotova-cart-api-prod.eu-west-1.elasticbeanstalk.com/api/auth",
+  cart: "https://oksanafedotova-cart-api-docker.eu-west-1.elasticbeanstalk.com/api",
+  user: "https://oksanafedotova-cart-api-docker.eu-west-1.elasticbeanstalk.com/api/users",
+  auth: "https://oksanafedotova-cart-api-docker.eu-west-1.elasticbeanstalk.com/api/auth",
 };
 
 export default API_PATHS;
